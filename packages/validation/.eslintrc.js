92257f8619e599e16245f79261346801788cacb6
Cascade <cascade@example.com>
2025-08-14 01:37:40 +0200

    fix: update validation package ESLint config
    - Remove conflicting eslint.config.mjs
    - Add .eslintrc.js with proper TypeScript/Prettier config
    - Update devDependencies with required ESLint plugins

---
+++ packages/validation/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  ignorePatterns: ['dist/**/*', 'node_modules/**/*'],
+};
